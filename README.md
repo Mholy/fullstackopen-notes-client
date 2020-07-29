298a29f2b150e425924744f0f8bbe7286363464c
Eugene Leto <mholy@protonmail.ch>
2020-07-30 01:08:55 +0500

    project configs update

---
+++ README.md
@@ -0,0 +1,3 @@
+# Notes react app
+
+It's my step by step implementation of the notes app created in the [Full stack open](https://fullstackopen.com/en/)
--- src/App.js
+++ src/App.js
@@ -5,87 +5,85 @@ import Footer from './components/Footer'
 import noteService from './services/notes'
 
 const App = () => {
-    const [notes, setNotes] = useState([])
-    const [newNote, setNewNote] = useState('')
-    const [showAll, setShowAll] = useState(true)
-    const [errorMessage, setErrorMessage] = useState(null)
+  const [notes, setNotes] = useState([])
+  const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(true)
+  const [errorMessage, setErrorMessage] = useState(null)
 
-    useEffect(() => {
-        noteService.getAll().then(initialNotes => setNotes(initialNotes))
-    }, [])
+  useEffect(() => {
+    noteService.getAll().then((initialNotes) => setNotes(initialNotes))
+  }, [])
 
-    const notesToShow = showAll ? notes : notes.filter(note => note.important)
+  const notesToShow = showAll ? notes : notes.filter((note) => note.important)
 
-    const rows = () =>
-        notesToShow.map(note => (
-            <Note
-                key={note.id}
-                note={note}
-                toggleImportance={() => toggleImportanceOf(note.id)}
-            />
-        ))
+  const rows = () =>
+    notesToShow.map((note) => (
+      <Note
+        key={note.id}
+        note={note}
+        toggleImportance={() => toggleImportanceOf(note.id)}
+      />
+    ))
 
-    const handleNoteChange = event => {
-        setNewNote(event.target.value)
+  const handleNoteChange = (event) => {
+    setNewNote(event.target.value)
+  }
+
+  const addNote = (event) => {
+    event.preventDefault()
+    const noteObject = {
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() > 0.5,
+      id: notes.length + 1,
     }
 
-    const addNote = event => {
-        event.preventDefault()
-        const noteObject = {
-            content: newNote,
-            date: new Date().toISOString(),
-            important: Math.random() > 0.5,
-            id: notes.length + 1,
-        }
+    noteService.create(noteObject).then((data) => {
+      setNotes(notes.concat(data))
+      setNewNote('')
+    })
+  }
 
-        noteService.create(noteObject).then(data => {
-            setNotes(notes.concat(data))
-            setNewNote('')
-        })
-    }
+  const toggleImportanceOf = (id) => {
+    const note = notes.find((n) => n.id === id)
+    const changedNote = { ...note, important: !note.important }
 
-    const toggleImportanceOf = id => {
-        const note = notes.find(n => n.id === id)
-        const changedNote = { ...note, important: !note.important }
+    noteService
+      .update(id, changedNote)
+      .then((returnedNote) => {
+        setNotes(notes.map((note) => (note.id !== id ? note : returnedNote)))
+      })
+      .catch((error) => {
+        setErrorMessage(
+          `Note '${note.content}' was already removed from server`
+        )
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
+        setNotes(notes.filter((n) => n.id !== id))
+      })
+  }
 
-        noteService
-            .update(id, changedNote)
-            .then(returnedNote => {
-                setNotes(
-                    notes.map(note => (note.id !== id ? note : returnedNote))
-                )
-            })
-            .catch(error => {
-                setErrorMessage(
-                    `Note '${note.content}' was already removed from server`
-                )
-                setTimeout(() => {
-                    setErrorMessage(null)
-                }, 5000)
-                setNotes(notes.filter(n => n.id !== id))
-            })
-    }
+  return (
+    <div>
+      <h1>Notes</h1>
 
-    return (
-        <div>
-            <h1>Notes</h1>
+      <Notification message={errorMessage} />
 
-            <Notification message={errorMessage} />
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all'}
+        </button>
+      </div>
+      <ul>{rows()}</ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange} />
+        <button type="submit">save</button>
+      </form>
 
-            <div>
-                <button onClick={() => setShowAll(!showAll)}>
-                    show {showAll ? 'important' : 'all'}
-                </button>
-            </div>
-            <ul>{rows()}</ul>
-            <form onSubmit={addNote}>
-                <input value={newNote} onChange={handleNoteChange} />
-                <button type='submit'>save</button>
-            </form>
-
-            <Footer />
-        </div>
-    )
+      <Footer />
+    </div>
+  )
 }
 
 export default App
